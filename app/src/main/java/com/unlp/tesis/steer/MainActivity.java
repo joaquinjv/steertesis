c9d8a3bc3b7b54467d3fea35545b46b1e24fc560
Pedro <pedroelbolson@hotmail.com>
2017-08-09 20:19:40 -0300

    Pedro Menu 3 audio switch

--- app/src/main/java/com/unlp/tesis/steer/MainActivity.java
+++ app/src/main/java/com/unlp/tesis/steer/MainActivity.java
@@ -27,6 +27,7 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Gravity;
@@ -34,6 +35,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -168,9 +170,6 @@ public class MainActivity extends AppCompatActivity implements
 //        titleNavView.setText(userData.getName());
         drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
         navView = (NavigationView)findViewById(R.id.navview);
-        if (Preferences.getAudioPreference(this)) {
-            navView.setCheckedItem(R.id.menu_audio_preference);
-        }
         navView.setNavigationItemSelectedListener(
                 new NavigationView.OnNavigationItemSelectedListener() {
                     @Override
@@ -330,6 +329,15 @@ public class MainActivity extends AppCompatActivity implements
     protected void onStart() {
         super.onStart();
         mAuth.addAuthStateListener(mAuthListener);
+        //Set audio preferences switch method
+        SwitchCompat switchItem = (SwitchCompat) navView.getMenu().findItem(R.id.menu_audio_preference_switch).getActionView();
+        switchItem.setOnCheckedChangeListener( new CompoundButton.OnCheckedChangeListener(){
+            @Override public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Preferences.setAudioPreference(getApplicationContext(), isChecked);
+            }
+        });
+        //Set audio preferences
+        switchItem.setChecked(Preferences.getAudioPreference(this));
         PreferenceManager.getDefaultSharedPreferences(this)
                 .registerOnSharedPreferenceChangeListener(this);
 
@@ -553,9 +561,6 @@ public class MainActivity extends AppCompatActivity implements
             case R.id.menu_seccion_1:
 //                fragment = new Fragment1();
 //                fragmentTransaction = true;
-                break;
-            case R.id.menu_audio_preference:
-                menuItem.setChecked(!menuItem.isChecked());
                 break;
         }
 
